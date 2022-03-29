e2df0a49971541bfc23a85a8a99a369cb1248ab0
ORxanone <recebovorxan3@gmail.com>
2022-03-28 23:08:26 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function call() {
+
+    var a = document.getElementById("call_back")
+
+    a.style.right = "-400px"
+
+    var b = document.getElementById("call_back_hidden")
+
+    b.style.right = "40px"
+
+    var c = document.getElementById("contact_fix")
+
+    c.style.right = "-400px"
+
+}
+
+function rClose() {
+
+    var a = document.getElementById("call_back")
+
+    a.style.right = "40px"
+
+    var b = document.getElementById("call_back_hidden")
+
+    b.style.right = "-400px"
+
+    var c = document.getElementById("contact_fix")
+
+    c.style.right = "-160px"
+
+}
+
+function rclick() {
+
+    var input = document.getElementById("input_number")
+
+    input.style.borderBottomColor = "red"
+
+}
+
+function ipt_click() {
+
+    var input = document.getElementById("input_number")
+
+    input.style.borderBottomColor = "black"
+
+}
